1e2da51e3403501e3778ba6e90dc3288b37ca211
UzmaFatima <128282240+uXmii@users.noreply.github.com>
2025-07-13 18:11:13 -0400

    firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,4 +1,4 @@
-// lib/firebase_options.dart - COMPLETE WITH YOUR REAL FIREBASE KEYS
+// lib/firebase_options.dart - CODE IS REDUCED AND KEYS ARE REMOVED AS THIS IS GOING TO BE DEPLOYED ON ANDROID
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
@@ -45,34 +45,6 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // ✅ ANDROID CONFIGURATION - YOUR ACTUAL VALUES
+  //  ANDROID CONFIGURATION - 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBPkMdGFm30eWKi5cBS4asc4IN81nrbzWc',
-    appId: '1:573188538666:android:36011cf9c3adfc0edede53',  // ✅ Your real Android App ID
-    messagingSenderId: '573188538666',
-    projectId: 'emoaid-fc5ce',
-    storageBucket: 'emoaid-fc5ce.appspot.com',
-  );
-
-  // iOS CONFIGURATION - You can add these later when you create iOS app
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBPkMdGFm30eWKi5cBS4asc4IN81nrbzWc',  // Usually different for iOS
-    appId: '1:573188538666:android:36011cf9c3adfc0edede53',  // You'll get this when adding iOS app
-    messagingSenderId: '573188538666',
-    projectId: 'emoaid-fc5ce',
-    storageBucket: 'emoaid-fc5ce.appspot.com',
-    iosClientId: 'your-ios-client-id.googleusercontent.com',  // You'll get this when adding iOS app
-    iosBundleId: 'com.bluethread.emoaid',
-  );
-
-  // ✅ WEB CONFIGURATION - YOUR ACTUAL VALUES
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBPkMdGFm30eWKi5cBS4asc4IN81nrbzWc',
-    appId: '1:573188538666:android:36011cf9c3adfc0edede53',  // You'll get this when adding Web app
-    messagingSenderId: '573188538666',
-    projectId: 'emoaid-fc5ce',
-    authDomain: 'emoaid-fc5ce.firebaseapp.com',
-    storageBucket: 'emoaid-fc5ce.appspot.com',
-    measurementId: 'G-XXXXXXXXXX',  // You'll get this with Google Analytics
-  );
-}
+    );
